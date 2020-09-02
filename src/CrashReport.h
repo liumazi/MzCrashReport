c99918c3acb843b03ce81d654e4b1f0436dc3bb5
liumazi <71892967@qq.com>
2020-09-02 09:59:48 +0800

    [修改]测试程序,使用参数指定触发错误

--- src/CrashReport.h
+++ src/CrashReport.h
@@ -8,40 +8,4 @@
 
 #include <windows.h>
 
-static const char* const rgBaseType[] =
-{
-	" <user defined> ",             // btNoType = 0,
-	" void ",                       // btVoid = 1,
-	" char* ",                      // btChar = 2,
-	" wchar_t* ",                   // btWChar = 3,
-	" signed char ",
-	" unsigned char ",
-	" int ",                        // btInt = 6,
-	" unsigned int ",               // btUInt = 7,
-	" float ",                      // btFloat = 8,
-	" <BCD> ",                      // btBCD = 9,
-	" bool ",                       // btBool = 10,
-	" short ",
-	" unsigned short ",
-	" long ",                       // btLong = 13,
-	" unsigned long ",              // btULong = 14,
-	" __int8 ",
-	" __int16 ",
-	" __int32 ",
-	" __int64 ",
-	" __int128 ",
-	" unsigned __int8 ",
-	" unsigned __int16 ",
-	" unsigned __int32 ",
-	" unsigned __int64 ",
-	" unsigned __int128 ",
-	" <currency> ",                 // btCurrency = 25,
-	" <date> ",                     // btDate = 26,
-	" VARIANT ",                    // btVariant = 27,
-	" <complex> ",                  // btComplex = 28,
-	" <bit> ",                      // btBit = 29,
-	" BSTR ",                       // btBSTR = 30,
-	" HRESULT "                     // btHresult = 31
-};
-
 bool InitCrashReport();
--- test/test/test.cpp
+++ test/test/test.cpp
@@ -8,7 +8,21 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	InitCrashReport();
 
-	*((int*)nullptr) = 6;
+	if (argc == 2)
+	{
+		int crashno = _tstoi(argv[1]);
+		printf("crashno %d\r\n", crashno);
+
+		switch (crashno)
+		{
+		case 0:
+			*((int*)6) = 6;
+			break;
+
+		default:
+			break;
+		}
+	}	
 
 	return 0;
 }
