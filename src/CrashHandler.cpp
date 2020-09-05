eba3f802e5d24067ebc183c74cf19977a45a0a73
liumazi <71892967@qq.com>
2020-09-06 01:18:35 +0800

    [增加]purecall和new_handler捕获

--- src/CrashHandler.cpp
+++ src/CrashHandler.cpp
@@ -4,10 +4,17 @@
 // Created by liuliu.mz on 20-08-28
 //
 
+#include <new.h>
+
 #include "CrashHandler.h"
 #include "CrashShow.h"
 
+#define MzExceptionCode 0x66666666
+
 static LPTOP_LEVEL_EXCEPTION_FILTER _SEH_Handler = nullptr;
+static _purecall_handler _PureCall_Handler = nullptr;
+static _PNH _New_Handler = nullptr;
+static _invalid_parameter_handler _InvalidParameter_Handler = nullptr;
 
 LONG WINAPI SEH_Handler(_In_ PEXCEPTION_POINTERS eps)
 {
@@ -16,9 +23,45 @@ LONG WINAPI SEH_Handler(_In_ PEXCEPTION_POINTERS eps)
 	return EXCEPTION_EXECUTE_HANDLER; // ÒÑ±¨¸æ ÍË°É
 }
 
+#if _MSC_VER >= 1300
+// C++ pure virtual call handler
+void PureCall_Handler()
+{
+	RaiseException(MzExceptionCode, 0, 0, nullptr);
+}
+
+int New_Handler(size_t sz)
+{
+	RaiseException(MzExceptionCode, 0, 0, nullptr);
+	return 0;
+}
+
+void(__cdecl *_invalid_parameter_handler)(const wchar_t *, const wchar_t *, const wchar_t *, unsigned int, uintptr_t);
+invalid_parameter_handler
+
+#endif 
+
 bool SetupCrashHanders()
 {
 	_SEH_Handler = SetUnhandledExceptionFilter(SEH_Handler);
+
+#if _MSC_VER >= 1300
+	// Catch pure virtual function calls.
+	// Because there is one _purecall_handler for the whole process, 
+	// calling this function immediately impacts all threads. The last 
+	// caller on any thread sets the handler. 
+	// http://msdn.microsoft.com/en-us/library/t296ys27.aspx
+	_PureCall_Handler = _set_purecall_handler(PureCall_Handler);
+
+	// Catch new operator memory allocation exceptions
+	_set_new_mode(1); // Force malloc() to call new handler too
+	_New_Handler = _set_new_handler(New_Handler);
+#endif
+
+#if _MSC_VER >= 1400
+	// Catch invalid parameter exceptions.
+	_InvalidParameter_Handler = _set_invalid_parameter_handler(InvalidParameterHandler);
+#endif
 
 	return (_SEH_Handler != nullptr);
 }
--- test/test/test.cpp
+++ test/test/test.cpp
@@ -1,9 +1,12 @@
 // test.cpp : 定义控制台应用程序的入口点。
 //
 
+#include <string>
+#include <vector>
+#include <assert.h>
+
 #include "stdafx.h"
 #include "CrashReport.h"
-#include <string>
 
 struct struct1
 {
@@ -11,9 +14,61 @@ struct struct1
 	int* p;
 };
 
-void fun1(struct1 s1[2], int ii[6], std::string s)
+void fun0(struct1 s1[2], int ii[6], std::string s)
 {
 	s1[0].aa = *s1[0].p;
+}
+
+void fun1()
+{
+	std::vector<int> a;
+
+	a.insert(a.begin(), 6);
+	a.insert(a.begin(), 66);
+	a.insert(a.begin(), 666);
+
+	auto itr = a.begin();
+	for (; itr != a.end(); itr++)
+	{
+		if (*itr == 6)
+		{
+			a.erase(itr);
+			//break; vector iterator not incrementable
+		}
+	}
+
+	if (itr == a.end()) // vector iterators incompatible
+	{
+
+	}
+}
+
+void fun2()
+{
+	assert(false);
+}
+
+class test3base
+{
+public:
+	test3base() { call_fun3(); }
+	void call_fun3() { this->fun3(); }
+	virtual void fun3() = 0;
+};
+
+class test3Dervied : public test3base
+{
+	virtual void fun3() { };
+};
+
+void fun3()
+{
+	test3Dervied t3;
+}
+
+void fun4()
+{
+	new int[0x1fffffff];
 }
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -31,9 +86,21 @@ int _tmain(int argc, _TCHAR* argv[])
 			struct1 test1[2];
 			test1[0].p = nullptr;
 			int ii[6];
-			fun1(test1, ii, "mz");
+			fun0(test1, ii, "mz");
 			//*((int*)6) = 6;
 			break;
+
+		case 1:
+			fun1();
+
+		case 2:
+			fun2();
+
+		case 3:
+			fun3();
+
+		case 4:
+			fun4();
 
 		default:
 			break;
